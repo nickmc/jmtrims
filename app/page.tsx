0a175eb6a92dc7b7f213e028b8e7230f3b4ce433
nick <nick.mcfarlane@gmail.com>
2026-07-14 17:38:39 +0200

    style: enlarge and center holding page text

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <p>coming soon JMTrims</p>;
+  return (
+    <p className="flex flex-1 items-center justify-center text-5xl">
+      coming soon JMTrims
+    </p>
+  );
 }
